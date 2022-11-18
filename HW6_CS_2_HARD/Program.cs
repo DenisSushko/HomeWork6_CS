b8d69d25361db388c7cadf830a64bd5483b0ad6f
Denis <help@denis-getcourse.ru>
2022-11-19 01:08:50 +0300

    HW6_CS_2_HARD

--- HW6_CS_2_HARD/Program.cs
+++ HW6_CS_2_HARD/Program.cs
@@ -3,52 +3,63 @@
 // Вывести на экран красивенько таблицей. Найти минимальное число и его индекс, найти максимальное число и его индекс, 
 // среднее арифметическое. Вывести эту информацию на экран.
 
-int[,] createArray(int m, int n) {
-    int[,] arr = new int [m, n];
+int[,] createArray(int m, int n)
+{
+    int[,] arr = new int[m, n];
     Random rnd = new Random();
-    for(int i = 0; i < m; i++) {
-        for (int j = 0; j < n; j++) {
-            arr[i,j] = rnd.Next(201) - 100;
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            arr[i, j] = rnd.Next(201) - 100;
         }
     }
     return arr;
 }
 
-void printArray(int [,] arr, int m, int n) {
-    for(int i = 0; i < m; i++) {
-        for (int j = 0; j < n; j++) {
-            Console.Write(arr[i,j] + " ");
+void printArray(int[,] arr, int m, int n)
+{
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            Console.Write(arr[i, j] + " ");
         }
         Console.WriteLine();
     }
 }
 
-double[] arrayStats (int[,] arr, int m, int n) {
-    int max = arr[0,0];
+double[] arrayStats(int[,] arr, int m, int n)
+{
+    int max = arr[0, 0];
     int maxIndex1 = 0;
     int maxIndex2 = 0;
 
-    int min = arr[0,0];
+    int min = arr[0, 0];
     int minIndex1 = 0;
     int minIndex2 = 0;
 
     double avg = 0;
 
-    double[] stats = new double [7];
+    double[] stats = new double[7];
 
-    for(int i = 0; i < m; i++) {
-        for (int j = 0; j < n; j++) {
-            if(arr[i,j] > max) {
-                max = arr[i,j];
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            if (arr[i, j] > max)
+            {
+                max = arr[i, j];
                 maxIndex1 = i;
                 maxIndex2 = j;
             }
-            if(arr[i,j] < min) {
-                min = arr[i,j];
+            if (arr[i, j] < min)
+            {
+                min = arr[i, j];
                 minIndex1 = i;
                 minIndex2 = j;
             }
-            avg += arr[i,j];
+            avg += arr[i, j];
         }
     }
 
@@ -67,7 +78,7 @@ Console.WriteLine("Введите m: ");
 int m = int.Parse(Console.ReadLine()!);
 Console.WriteLine("Введите n: ");
 int n = int.Parse(Console.ReadLine()!);
-int[,] arr = createArray(m,n);
+int[,] arr = createArray(m, n);
 printArray(arr, m, n);
 double[] stats = arrayStats(arr, m, n);
 Console.WriteLine($"Максимальное число: {stats[0]}, его индекс: ({stats[1]}, {stats[2]})");
