4a05e52305f2e5ffe36db03944e4d07bb13313ed
Denis <help@denis-getcourse.ru>
2022-11-19 01:09:43 +0300

    HW_CS_40_HARD

--- HW_CS_40_HARD/Program.cs
+++ HW_CS_40_HARD/Program.cs
@@ -4,8 +4,9 @@
 // Если да, то вывести всю информацию по нему - площадь, периметр, 
 // значения углов треугольника в градусах, является ли он прямоугольным, равнобедренным, равносторонним.
 
-double[] InputTriangle() {
-    double[] triangle = new double [3];
+double[] InputTriangle()
+{
+    double[] triangle = new double[3];
     Console.WriteLine("Введите первую сторону треугольника: ");
     triangle[0] = double.Parse(Console.ReadLine()!);
     Console.WriteLine("Введите вторую сторону треугольника: ");
@@ -15,72 +16,87 @@ double[] InputTriangle() {
     return triangle;
 }
 
-bool checkTriangle(double[] triangle) {
+bool checkTriangle(double[] triangle)
+{
     bool isTriangle = false;
-    if (triangle[0] + triangle[1] > triangle[2] && triangle[0] + triangle[2] > triangle[1] && triangle[1] + triangle[2] > triangle[0]) {
+    if (triangle[0] + triangle[1] > triangle[2] && triangle[0] + triangle[2] > triangle[1] && triangle[1] + triangle[2] > triangle[0])
+    {
         Console.WriteLine("Такой треугольник может существовать");
         isTriangle = true;
     }
-    else {
+    else
+    {
         Console.WriteLine("Такой треугольник НЕ может существовать");
     }
     return isTriangle;
 }
 
-void triangleStats (double[] triang) {
+void triangleStats(double[] triang)
+{
     double P = (triang[0] + triang[1] + triang[2]); // Периметр
-    double p = P/2; // Полупериметр
-    double S = Math.Round(Math.Sqrt(p*(p - triang[0]) * (p - triang[1]) * (p - triang[2])), 0); // Площадь
+    double p = P / 2; // Полупериметр
+    double S = Math.Round(Math.Sqrt(p * (p - triang[0]) * (p - triang[1]) * (p - triang[2])), 0); // Площадь
 
-    double[] angles = new double [3];
+    double[] angles = new double[3];
     double cosAlpha = ((triang[0] * triang[0]) + (triang[2] * triang[2]) - (triang[1] * triang[1])) / (2 * triang[0] * triang[2]); //Косинус угла альфа
-    double cosBeta = ((triang[0] * triang[0]) + (triang[1] * triang[1]) - (triang[2] * triang[2]))  / (2 * triang[0] * triang[1]); //Косинус угла бета
+    double cosBeta = ((triang[0] * triang[0]) + (triang[1] * triang[1]) - (triang[2] * triang[2])) / (2 * triang[0] * triang[1]); //Косинус угла бета
     angles[0] = Math.Round((Math.Acos(cosAlpha)) * 180 / Math.PI, 0); //угол Альфа в градусах
     angles[1] = Math.Round((Math.Acos(cosBeta)) * 180 / Math.PI, 0); //угол Бета в градусах
     angles[2] = 180 - angles[1] - angles[0]; // угол Гамма в градусах
 
-    
+
     bool isRectangular = false;
-    for(int i = 0; i < 3; i++) {
-        if (angles[i] == 90) {
+    for (int i = 0; i < 3; i++)
+    {
+        if (angles[i] == 90)
+        {
             isRectangular = true;
             break;
         }
     }
 
     bool isIsoscles = false; //Равнобедренный или нет
-    if (triang[0] == triang[1] || triang[0] == triang[2] || triang[1] == triang[2]) {
+    if (triang[0] == triang[1] || triang[0] == triang[2] || triang[1] == triang[2])
+    {
         isIsoscles = true;
     }
 
     bool isEquilateral = false; // Равносторонний или нет
-    if (triang[0] == triang[1] && triang[1] == triang[2]) {
+    if (triang[0] == triang[1] && triang[1] == triang[2])
+    {
         isEquilateral = true;
     }
 
     Console.WriteLine($"Периметр = {P}, площадь = {S}");
     Console.WriteLine($"Угол между сторонами a и c = {angles[0]}, угол между сторонами a и b = {angles[1]}, угол между сторонами b и c = {angles[2]}");
-    if(isRectangular) {
+    if (isRectangular)
+    {
         Console.WriteLine("Треугольник - прямоугольный");
     }
-    else {
+    else
+    {
         Console.WriteLine("Треугольник - НЕ прямоугольный");
     }
-    if(isIsoscles) {
+    if (isIsoscles)
+    {
         Console.WriteLine("Треугольник - равнобедренный");
     }
-    else {
+    else
+    {
         Console.WriteLine("Треугольник - НЕ равнобедренный");
     }
-    if(isEquilateral) {
+    if (isEquilateral)
+    {
         Console.WriteLine("Треугольник - равносторонний");
     }
-    else {
+    else
+    {
         Console.WriteLine("Треугольник - НЕ равносторонний");
     }
 }
 
 double[] triangle = InputTriangle();
-if(checkTriangle(triangle)) {
+if (checkTriangle(triangle))
+{
     triangleStats(triangle);
 }
